a85e7cb826d864da644d42c355ad3c100c663827
Jairo Honorio <registrofx@gmail.com>
2015-02-28 17:30:36 +0000

    Gulp integrated

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,76 @@
-var gulp       = require('gulp');
-var requireDir = require('require-dir');
+var browserify = require('browserify');
+var gulp = require('gulp');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var uglify = require('gulp-uglify');
+var jshint = require('gulp-jshint');
+var watch = require('gulp-watch');
+var notify = require("gulp-notify");
+var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync');
 
-/**
- * Require all local grunt tasks.
- */
-requireDir('./tasks');
+var getBundleName = function () {
+    var version = require('./package.json').version;
+    var name = require('./package.json').name;
+    return name + '.' + version + '.' + 'min';
+};
 
-/**
- * Log all task errors for debugging.
- */
-gulp.on('task_err', function (taskError) {
-  console.error(taskError.err.stack);
+var baseFile = './languages/index.js';
+
+gulp.task('watch', function () {
+    watch(['./{bin,languages}/*.js', './languages/**/*.{js,swig}'], function () {
+        notify({message: 'building'});
+        gulp.start('build');
+    });
+});
+
+gulp.task('watch:dist', function () {
+    watch(['./dist/*.js', './test/*.html', './www/**/*.raml'], function () {
+        notify({message: 'reloading'});
+        browserSync.reload();
+    });
+});
+
+gulp.task('lint', function () {
+    return gulp.src(['./{bin,languages}/*.js', './languages/**/*.js'])
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+gulp.task('build', ['lint'], function () {
+
+    var bundler = browserify([baseFile], {
+        debug: true
+    });
+
+    bundler.require(baseFile, {expose: 'raml-ws-generators'});
+    bundler.transform('folderify');
+
+    var bundle = function () {
+        return bundler
+            .bundle()
+            .pipe(source(getBundleName() + '.js'))
+            .pipe(buffer())
+            .pipe(sourcemaps.init({loadMaps: true}))
+            .pipe(uglify())
+            .pipe(sourcemaps.write('./'))
+            .pipe(gulp.dest('./dist/'));
+    };
+
+    return bundle();
+});
+
+gulp.task('serve', ['watch:dist'], function () {
+
+    browserSync({
+        server: {
+            baseDir: './',
+            index: 'www/index.html'
+        }
+    });
+
+});
+
+gulp.task('default', ['build', 'watch', 'serve'], function () {
+
 });
