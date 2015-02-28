993bbd610461dc8191a409d9eed38f24020bd824
Jairo Honorio <registrofx@gmail.com>
2015-02-28 16:11:52 +0000

    Agregado gitignore y quitados archivos que no debian

--- output/api.php
+++
@@ -1,7 +0,0 @@
- 
- $app->get('/songs', 'APIrequest', function () use ($app) {
-        //---RAML-PARSER-USER-AREA-START---
-
-        $app->render(200, ['msg','ok']);
-        //---RAML-PARSER-USER-AREA-END---
-    });
